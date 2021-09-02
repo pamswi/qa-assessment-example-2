8647d7cc48fbd50e74dcbe4e67826776e58b7fe2
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2021-09-02 08:47:12 +0100

    Update questions.py

--- programs/questions.py
+++ programs/questions.py
@@ -93,15 +93,4 @@ def five(chars):
     # six('hello world, how are you?', 20) → ['hello world, how are', 'you?']
     
 def six(paragraph, limit):
-    wordlist = paragraph.split(' ')
-    pagelist = []
-    page = ''
-    while wordlist != []:
-        if len(page) + len(wordlist[0]) <= limit:
-            page += (wordlist[0] + ' ')
-            wordlist.remove(wordlist[0])
-        else:
-            pagelist.append(page[0:-1])
-            page = ''
-    pagelist.append(page[0:-1])
-    return pagelist
+    pass
